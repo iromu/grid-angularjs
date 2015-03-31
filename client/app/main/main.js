2ff298179932ea6450a2e7d79682e67cbb295473
Iván Rodríguez Murillo <wantez@gmail.com>
2015-03-31 20:38:59 +0200

    First commit

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('parallelJsApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
