da9bb183c304bb8b9dc944a1bfaefb2e5e47ce77
Iván Rodríguez Murillo <wantez@gmail.com>
2015-05-06 21:06:49 +0200

    Seeding

--- server/config/seed.js
+++ server/config/seed.js
@@ -13,49 +13,60 @@ var Canvas = require('canvas');
 var Image = Canvas.Image;
 var fs = require('fs');
 
-var imageName = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 36);
-
+var LENA_PNG = __dirname + '/../api/pixel/lena.png';
 var LENA_JSON = __dirname + '/../api/pixel/lena.json';
+var NN_X = __dirname + '/../api/nn/X.txt';
+
+var readJson = function (json) {
+  var data = fs.readFileSync(json);
+  return JSON.parse(data);
+};
+
+var readNnXFile = function () {
+  var data = fs.readFileSync(NN_X);
+  return JSON.parse(data);
+};
+
 
 var readLenaFile = function () {
-  var data = fs.readFileSync(LENA_JSON);
-  var arr = JSON.parse(data);
-  Pixel.collection.insert(arr, function () {
-    console.log('finished populating pixels from ' + LENA_JSON);
-  });
-};
-Pixel.find({}).remove(function () {
 
-    if (!fs.existsSync(LENA_JSON)) {
+//  console.log('Read file ' + __dirname + LENA_PNG);
+  var lena = fs.readFileSync(LENA_PNG);
+  var img = new Image;
+  img.src = lena;
 
-      var lena = fs.readFileSync(__dirname + '/../api/pixel/lena.png');
-      var img = new Image;
-      img.src = lena;
+  var canvas = new Canvas(img.width, img.height);
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0, img.width, img.height);
 
-      var canvas = new Canvas(img.width, img.height);
-      var ctx = canvas.getContext('2d');
-      ctx.drawImage(img, 0, 0, img.width, img.height);
-
-
-      var pixels = [];
-      for (var x = 0; x < img.width; x++) {
-        for (var y = 0; y < img.height; y++) {
-          var d = ctx.getImageData(x, y, 1, 1);
-          var pixel = {x: x, y: y, r: d.data[0], g: d.data[1], b: d.data[2], a: d.data[3], image: imageName};
-          //Pixel.create(pixel);
-          pixels.push(pixel);
-        }
-      }
-
-      Pixel.collection.insert(pixels, {multi: true}, function () {
-        fs.writeFileSync(LENA_JSON, JSON.stringify(pixels));
-      });
-
-    } else
-      readLenaFile();
+  var pixels = [];
+  for (var x = 0; x < img.width; x++) {
+    for (var y = 0; y < img.height; y++) {
+      var d = ctx.getImageData(x, y, 1, 1);
+      var pixel = {x: x, y: y, r: d.data[0], g: d.data[1], b: d.data[2], image: 'lena.png'};
+      pixels.push(pixel);
+    }
   }
-)
-;
+  return pixels;
+};
+
+
+Pixel.find({}).remove(function () {
+  var pixels = [];
+  var save = false;
+  if (!fs.existsSync(LENA_JSON)) {
+    console.log('Reading png file');
+    pixels = readLenaFile();
+    save = true;
+  } else {
+    console.log('Reading json file');
+    pixels = readJson(LENA_JSON);
+  }
+  Pixel.collection.insert(pixels, {multi: true}, function () {
+    if (save) fs.writeFileSync(LENA_JSON, JSON.stringify(pixels));
+    console.log('finished populating ' + pixels.length + ' pixels for lena.png');
+  });
+});
 
 User.find({}).remove(function () {
   User.create({
