0123c9bc3ef32ef866772869c8fda01e793de81c
Iván Rodríguez Murillo <wantez@gmail.com>
2015-05-06 22:40:27 +0200

    nn endpoint

--- server/routes.js
+++ server/routes.js
@@ -9,6 +9,7 @@ var errors = require('./components/errors');
 module.exports = function(app) {
 
   // Insert routes below
+  app.use('/api/nn', require('./api/nn'));
   app.use('/api/pixels', require('./api/pixel'));
   app.use('/api/users', require('./api/user'));
 
