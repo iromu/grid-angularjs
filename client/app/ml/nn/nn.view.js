b8eb474a897c167c773fbce5704e09def158c086
Iván Rodríguez Murillo <wantez@gmail.com>
2015-05-10 13:47:32 +0200

    nn auto training

--- client/app/ml/nn/nn.view.js
+++ client/app/ml/nn/nn.view.js
@@ -2,22 +2,40 @@
   'use strict';
 
   angular.module('gridApp')
-    .factory('nnViewService', function (canvasViewService) {
+    .factory('nnView', function (canvasViewService) {
 
       // Public API here
       return {
         initView: function (cb) {
           canvasViewService.loadImage('#numbersCanvas', 'assets/images/numbers-bw.png', function () {
-            canvasViewService.clearImage('#numberCanvas');
+            canvasViewService.clearImage('#trainingCanvas');
             cb();
           });
         },
-        loadRegion: function (region) {
-          var imageData = canvasViewService.getRegion('#numbersCanvas', region, 30);
-          canvasViewService.setImageData('#numberCanvas', imageData);
+        loadRegionForTraining: function (index) {
+          var region = canvasViewService.getRegion('#numbersCanvas', index, 20);
+          canvasViewService.setImageData('#trainingCanvas', region.imageData);
+          canvasViewService.drawSelection('#numbersCanvasOverlay', region.selection);
+          return region.imageData;
+        },
+        loadRegionForPrediction: function (index) {
+          var region = canvasViewService.getRegion('#numbersCanvas', index, 20);
+          canvasViewService.setImageData('#predictionCanvas', region.imageData);
+          canvasViewService.drawSelection('#numbersCanvasOverlay', region.selection, 'cyan');
+          return region.imageData;
         },
         getRegionData: function (region) {
-        return  canvasViewService.getRegion('#numbersCanvas', region, 30).data;
+          return canvasViewService.getRegion('#numbersCanvas', region, 20).imageData.data;
+        },
+        onNextNumber: function () {
+          $('#input-pick-control').focus();
+          $('#input-pick-control').select();
+        },
+        onTrainNetwork: function () {
+          $('#trainButton').prop('disabled', true);
+        },
+        onConfigureNetwork: function () {
+          $('#trainButton').prop('disabled', false);
         }
       };
 
--- client/components/canvas/canvas.service.spec.js
+++ client/components/canvas/canvas.service.spec.js
@@ -11,8 +11,115 @@ describe('Service: canvasViewService', function () {
     canvasViewService = _canvasViewService_;
   }));
 
-  it('should do something', function () {
+  it('should exist', function () {
     expect(!!canvasViewService).toBe(true);
+  });
+
+  it('should calcWindowSlice', function () {
+    expect(canvasViewService.calcWindowSlice(1, 200, 200, 20)).toEqual({
+      region: 1,
+      size: 20,
+      col: 1,
+      row: 1,
+      x: 0,
+      y: 0
+    });
+    expect(canvasViewService.calcWindowSlice(2, 200, 200, 20)).toEqual({
+      region: 2,
+      size: 20,
+      col: 2,
+      row: 1,
+      x: 19,
+      y: 0
+    });
+    expect(canvasViewService.calcWindowSlice(9, 200, 200, 20)).toEqual({
+      region: 9,
+      size: 20,
+      col: 9,
+      row: 1,
+      x: 159,
+      y: 0
+    });
+    expect(canvasViewService.calcWindowSlice(10, 200, 200, 20)).toEqual({
+      region: 10,
+      size: 20,
+      col: 10,
+      row: 1,
+      x: 179,
+      y: 0
+    });
+    expect(canvasViewService.calcWindowSlice(11, 200, 200, 20)).toEqual({
+      region: 11,
+      size: 20,
+      col: 1,
+      row: 2,
+      x: 0,
+      y: 19
+    });
+    expect(canvasViewService.calcWindowSlice(12, 200, 200, 20)).toEqual({
+      region: 12,
+      size: 20,
+      col: 2,
+      row: 2,
+      x: 19,
+      y: 19
+    });
+    expect(canvasViewService.calcWindowSlice(20, 200, 200, 20)).toEqual({
+      region: 20,
+      size: 20,
+      col: 10,
+      row: 2,
+      x: 179,
+      y: 19
+    });
+    expect(canvasViewService.calcWindowSlice(31, 200, 200, 20)).toEqual({
+      region: 31,
+      size: 20,
+      col: 1,
+      row: 4,
+      x: 0,
+      y: 59
+    });
+    expect(canvasViewService.calcWindowSlice(41, 200, 200, 20)).toEqual({
+      region: 41,
+      size: 20,
+      col: 1,
+      row: 5,
+      x: 0,
+      y: 79
+    });
+    expect(canvasViewService.calcWindowSlice(30, 200, 200, 20)).toEqual({
+      region: 30,
+      size: 20,
+      col: 10,
+      row: 3,
+      x: 179,
+      y: 39
+    });
+    expect(canvasViewService.calcWindowSlice(40, 200, 200, 20)).toEqual({
+      region: 40,
+      size: 20,
+      col: 10,
+      row: 4,
+      x: 179,
+      y: 59
+    });
+    expect(canvasViewService.calcWindowSlice(50, 200, 200, 20)).toEqual({
+      region: 50,
+      size: 20,
+      col: 10,
+      row: 5,
+      x: 179,
+      y: 79
+    });
+    expect(canvasViewService.calcWindowSlice(51, 200, 200, 20)).toEqual({
+      region: 51,
+      size: 20,
+      col: 1,
+      row: 6,
+      x: 0,
+      y: 99
+    });
   });
 
 });
